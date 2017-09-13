649b68228073025da93eea6c05162bf7c44503ce
Lila <lur159@psu.edu>
2017-09-13 18:27:19 -0400

    consolidated scripts

---
+++ Fig2/fig2.sh
@@ -0,0 +1,12 @@
+set -e
+
+python ../scripts/quantile_norm_singlebase_bin.py tab_files 
+
+CDT_DIR=_CDT
+
+if [ ! -d $CDT_DIR ]
+	then
+		python ../scripts/map_shifted_tags_to_ref.py -u 2000 -d 2000 tab_files ALL-RP-SAGA-TFIID-SUT-CUT-XUT_TSS-TES-MID_sortedby_geneLength.gff
+fi
+
+python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR ALL-RP-SAGA-TFIID-SUT-CUT-XUT_TSS-TES-MID_sortedby_geneLength.gff 
--- Fig2/fig2a.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-python ../scripts/quantile_norm_singlebase_bin.py tab_files 
-python ../scripts/map_shifted_tags_to_ref.py -u 2000 -d 2000 tab_files/Normalized_tab_files 
-python ../scripts/sort_CDT_by_given_file.py -o 2 tab_files/Normalized_tab_files/_CDT
