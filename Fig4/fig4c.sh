48384d24433a9e5b4a2168620af0ce4fe3273399
Lila <lur159@psu.edu>
2017-09-13 18:54:28 -0400

    created script

---
+++ Fig4/fig4c.sh
@@ -0,0 +1,17 @@
+set -e
+
+python ../scripts/quantile_norm_singlebase_bin.py tab_files 
+
+CDT_DIR=_CDT
+
+if [ ! -d $CDT_DIR ]
+	then
+		python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 tab_files/Normalized_tab_files  Hsf1-union-Xu-TSS-divergent-downstream.gff
+fi
+
+if [ ! -d $CDT_DIR ]
+	then
+		python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 tab_files/Normalized_tab_files  Hsf1-union-Xu-TSS-divergent-upstream.gff
+fi
+
+python ../scripts/extract_tag_occupancy.py $FN 50 50	#TODO: what is fn??
