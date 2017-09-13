28044419b71af1c9b053b3c84da8c85ba289333d
Lila <lur159@psu.edu>
2017-09-13 18:45:50 -0400

    fixed paths

--- Fig3/fig3.sh
+++ Fig3/fig3.sh
@@ -7,7 +7,7 @@ SORT_FILE=sorted_list-RPG_SAGA-act-rep-no_TFIID-act-rep-no.txt
 
 if [ ! -d $CDT_DIR ]
 	then
-		python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 tab_files $SORT_FILE
+		python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 tab_files/Normalized_tab_files $SORT_FILE
 fi
 
 python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR $SORT_FILE
--- Fig4/fig4a.sh
+++ Fig4/fig4a.sh
@@ -6,7 +6,7 @@ CDT_DIR=_CDT
 
 if [ ! -d $CDT_DIR ]
 	then
-		python ../scripts/map_shifted_tags_to_ref.py -u 1000 -d 1000 tab_files Hsf1-union-Xu-TSS-divergent-upstream.gf
+		python ../scripts/map_shifted_tags_to_ref.py -u 1000 -d 1000 tab_files/Normalized_tab_files  Hsf1-union-Xu-TSS-divergent-upstream.gff
 fi
 
 python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR Hsf1-union-Xu-TSS-sortby-distance.gff
