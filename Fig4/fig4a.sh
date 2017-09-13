4520f3967b1789bca05485693f4c03a8f8c9588d
Lila <lur159@psu.edu>
2017-09-13 18:35:05 -0400

    created script

---
+++ Fig4/fig4a.sh
@@ -0,0 +1,12 @@
+set -e
+
+python ../scripts/quantile_norm_singlebase_bin.py tab_files 
+
+CDT_DIR=_CDT
+
+if [ ! -d $CDT_DIR ]
+	then
+		python ../scripts/map_shifted_tags_to_ref.py -u 1000 -d 1000 tab_files Hsf1-union-Xu-TSS-divergent-upstream.gf
+fi
+
+python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR Hsf1-union-Xu-TSS-sortby-distance.gff
