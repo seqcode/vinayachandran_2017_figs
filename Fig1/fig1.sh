fce3231b61f6bd5ddc775fc0cd6299c4f4b72498
Lila <lur159@psu.edu>
2017-09-20 15:59:36 -0400

    fixed tab files

--- Fig1/fig1.sh
+++ Fig1/fig1.sh
@@ -1,27 +1,25 @@
 set -e
 
-WD=$PWD
-
-SHARED_FILES=../shared_files
-if [ ! -d $SHARED_FILES ]
+if [ ! -d tab_files ]
 	then
-		mkdir $SHARED_FILES
+		mkdir tab_files
+		cat ../shared_files/Figure_Script_Table.csv | awk -F "," '$4 == 1 {print $11}' > tab_files_list
+		python ../scripts/fetch_from_gpfs.py Fig1
 fi
 
-CHROM_INFO=../shared_files/sacCer3.chrom.sizes
-if [ ! -e $CHROM_INFO ]
+for f in tab_files/*.gz
+do
+	gunzip $f
+done
+
+if [ ! -d tab_files/Normalized_tab_files ]
 	then
-		cd $SHARED_FILES
-		wget http://hgdownload-test.cse.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.chrom.sizes
-		cd $WD
+		python ../scripts/quantile_norm_singlebase_bin.py tab_files ../shared_files/sacCer3.chrom.sizes --stranded
 fi
 
-CDT_DIR=_CDT
-
-if [ ! -d $CDT_DIR ]
+if [ ! -d tab_files/_CDT ]
 	then
 		python ../scripts/map_shifted_tags_to_ref.py tab_files/Normalized_tab_files ../shared_files/Xu_2009_ORF_TSS_TES_V64.gff
 fi
 
-python ../scripts/quantile_norm_singlebase_bin.py tab_files $CHROM_INFO --stranded
 python ../scripts/Composite_for_many_factors_one_plot.py -w 20 tab_files/Normalized_tab_files 
