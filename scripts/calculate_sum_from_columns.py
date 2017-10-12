17148615e7e29fba0c9db14110dbb43fa4646f8d
Lila <lur159@psu.edu>
2017-10-12 14:54:51 -0400

    debugged

--- scripts/calculate_sum_from_columns.py
+++ scripts/calculate_sum_from_columns.py
@@ -3,16 +3,16 @@
 usage = """
 Usage:
 It calculates the sum of the range of data relative to your coor
+Takes CDTs as input
 
 ***
 <required>:
-suffix for all the input files
 where the coordinate of your reference point (ex. if this dataset is mapped to -500 to 1000 relative to TSS, then this is 500)
 upstream limit
 downstream limit
 output file
 
-Example: python calculate_sum_from_columns.py .tab 500 -150 150 outfile 
+Example: python calculate_sum_from_columns.py 500 -150 150 outfile 
 ***
 
 Created by Kuangyu Yen on 2012-04-27.
@@ -54,8 +54,6 @@ def calculate_sum(infile, left, right):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('suffix',
-                        help='Suffix of files to process')
   parser.add_argument('ref_point', type=int,
                         help='Reference point')
   parser.add_argument('up_lim', type=int,
@@ -68,13 +66,13 @@ if __name__ == '__main__':
   args = parser.parse_args()   
 
   left, right = args.ref_point + args.up_lim, args.ref_point + args.dn_lim
-  infiles, dict_exp_value, result = getFiles(suffix), {}, []
+  infiles, dict_exp_value, result = getFiles(".cdt"), {}, []	#only compatible with CDTs
   
   for infile in infiles:
     genelist, values = calculate_sum(infile, left, right)
     dict_exp_value[infile] = values
     
-    print "finish processing {}".format(infile)
+    print "finished processing {}".format(infile)
   
   i = 0
   while i < len(genelist):
@@ -86,10 +84,9 @@ if __name__ == '__main__':
     result.append(tmp)
     i += 1
 
-  
   exp_name = dict_exp_value.keys()
   exp_name.insert(0, "gene")
-  writer = csv.writer(open(out_file, "w"), delimiter="\t")
+  writer = csv.writer(open(args.out_file, "w"), delimiter="\t")
   writer.writerow(exp_name)
   for row in result:
     writer.writerow(row)
