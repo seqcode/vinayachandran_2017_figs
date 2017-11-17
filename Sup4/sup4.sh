a1e0a30381735b1da3ad999066e8a5edbb3bc23c
Lila <lur159@psu.edu>
2017-11-17 13:38:12 -0500

    fixed paths

--- Sup4/sup4.sh
+++ Sup4/sup4.sh
@@ -2,12 +2,13 @@ set -e
 
 sh ../scripts/get_chrom_sizes.sh
 
-GFF=TFIID-nochange_TSS_Xu_2009_ORF_PURE_SET.gff
+GFF=TFIID_TSS_Xu_2009_ORF_Ts_V64.gff	
 
-if [ ! -e $GFF ]
+if [ ! -e ../shared_files/$GFF ]
 	then
 		wget 	#TODO: add url
-		gunzip $GFF.gz
+		mv $GFF.gz ../shared_files
+		gunzip ../shared_files/$GFF.gz
 fi
 
 ALL_TAB=../GSE98573_RAW
@@ -31,10 +32,10 @@ if [ ! -d $TAB_DIR ]
 fi
 
 #get TFIIH occupancy
-python ../scripts/extract_tag_occupancy.py 19325sacCer3.tab $GFF ../shared_files/sacCer3.chrom.sizes TFIIH_occupancy.txt 100 100
+python ../scripts/extract_tag_occupancy.py 19325sacCer3.tab ../shared_files/$GFF ../shared_files/sacCer3.chrom.sizes TFIIH_occupancy.txt 100 100
 
 #sort occupancy file
-cat $GFF | awk '{print $9}' > genes.txt
+cat ../shared_files/$GFF | awk '{print $9}' > genes.txt
 paste genes.txt TFIIH_occupancy.txt > TFIIH_occupancy_by_gene.tsv
 sort -k 2 -n TFIIH_occupancy_by_gene.tsv | awk '{print $1}' > sorted_TFIIH_genes.tsv
 QUARTER_LENGTH=$(($(cat sorted_TFIIH_genes.tsv | wc -l)/4))
@@ -42,8 +43,8 @@ cat sorted_TFIIH_genes.tsv | head -$QUARTER_LENGTH > bottom_25percent.txt
 cat sorted_TFIIH_genes.tsv | tail -$QUARTER_LENGTH > top_25percent.txt
 
 #convert gene lists to GFF
-python genes_from_gff.py $GFF bottom_25percent.txt
-python genes_from_gff.py $GFF top_25percent.txt
+python genes_from_gff.py ../shared_files/$GFF bottom_25percent.txt
+python genes_from_gff.py ../shared_files/$GFF top_25percent.txt
 
 #map to each GFF
 python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 -o bottom25 tab_files bottom_25percent.gff
