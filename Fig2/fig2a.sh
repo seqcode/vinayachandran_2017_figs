8793597e3ebbdebf66219e230a7f0749175b49c4
Lila <lur159@psu.edu>
2017-12-20 10:26:53 -0500

    fixed paths

--- Fig2/fig2a.sh
+++ Fig2/fig2a.sh
@@ -1,12 +1,19 @@
 set -e
 
-sh ../scripts_get_chrom_sizes.sh
+sh ../scripts/get_chrom_sizes.sh
 
-ALL_TAB=../GSE98573_RAW
+TAB_DIR=tab_files_a
+TAR=GSE98573_RAW.tar
 
-if [ ! -d $ALL_TAB ]
+if [ ! -d $TAB_DIR ]
 	then 
-		tar xvf $ALL_TAB.tar
+		mkdir $TAB_DIR	
+		mv $TAR $TAB_DIR
+		cd $TAB_DIR
+		tar xvf $TAR
+		rm $TAR
+		gunzip *.gz
+		cd ..
 fi
 
 GFF=ALL-RP-SAGA-TFIID-SUT-CUT-XUT_TSS-TES-MID_sortedby_geneLength.gff
@@ -18,18 +25,18 @@ if [ ! -e ../shared_files/$GFF ]
 		gunzip ../shared_files/$GFF.gz
 fi
 
-IDS=(50416 50513 50501 50525 53319 50428 50531 50519 54513 51825 53803)
-TAB_DIR=tab_files_a
+#IDS=(50416 50513 50501 50525 53319 50428 50531 50519 54513 51825 53803)
+#TAB_DIR=tab_files_a
 
-if [ ! -d $TAB_DIR ]
-	then
-		mkdir $TAB_DIR
-
-		for ID in "${IDS[@]}"
-		do
-			cp $ALL_TAB/$ID"sacCer3".tab $TAB_DIR
-		done
-fi
+#if [ ! -d $TAB_DIR ]
+#	then
+#		mkdir $TAB_DIR
+#
+#		for ID in "${IDS[@]}"
+#		do
+#			cp $ALL_TAB/$ID"sacCer3".tab $TAB_DIR
+#		done
+#fi
 
 NORM_DIR=$TAB_DIR/Normalized_tab_files
 
