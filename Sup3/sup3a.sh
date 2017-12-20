be7900deb0d4963de6d1346f63a98771b27587f9
Lila <lur159@psu.edu>
2017-12-20 16:10:41 -0500

    fixed paths

--- Sup3/sup3a.sh
+++ Sup3/sup3a.sh
@@ -1,21 +1,25 @@
 set -e
 
-sh ../get_chrom_sizes.sh
-
-ALL_TAB=../GSE98573_RAW
-
-if [ ! -d $ALL_TAB ]
-	then 
-		tar xvf $ALL_TAB.tar
-fi
+sh ../scripts/get_chrom_sizes.sh
 
 TAB_DIR=tab_files_a
+TAR=GSE98573_RAW.tar
+
+if [ ! -d $TAB_DIR ]
+	then 
+		mkdir $TAB_DIR	
+		mv $TAR $TAB_DIR
+		cd $TAB_DIR
+		tar xvf $TAR
+		rm $TAR
+		gunzip *.gz
+		cd ..
+fi
 
 FACTORS=(Spt3 Spt15 Sua7 Taf1 Ssl2 Rpb3 Hsf1)
 YLIMS=(3000 2500 2000 900 2000 400 1600)
-ALL_IDS=([50525 50526 50527 50528 50529 50530] [53319 53320 53321] [50428 50429 50430] [50531 50532 50533 50534 50535 50536] [50519 50520 50521 50522 50523 50524] [51835 51836 51837 51838 51839 51840] [53301 59801 59804 53302 59802 59805 53303 59803 59806])
+ALL_IDS=(50525,50526,50527,50528,50529,50530 53319,53320,53321 50428,50429,50430 50531,50532,50533,50534,50535,50536 50519,50520,50521,50522,50523,50524 51835,51836,51837,51838,51839,51840 59804,59805,59806)
 CONDITIONS=(activated nochange repressed_rp)
-URLS=()	#TODO: add
 
 for i in `seq 0 $((${#FACTORS[@]}-1))`
 do
@@ -24,13 +28,14 @@ do
 	FACTOR_DIR=$TAB_DIR/$FACTOR
 	NORM_DIR=$FACTOR_DIR/Normalized_tab_files
 	IDS=${ALL_IDS[$i]}
+	IDS_ARRAY=(${IDS//,/ })
 	
 	if [ ! -d $FACTOR_DIR ]
 		then
 			mkdir -p $FACTOR_DIR
-			for ID in "${IDS[@]}"
+			for ID in "${IDS_ARRAY[@]}"
 			do
-				cp $ALL_TAB/$ID"sacCer3".tab $FACTOR_DIR
+				mv $TAB_DIR/*_$ID"sacCer3".tab $FACTOR_DIR
 			done
 	fi
 
@@ -41,20 +46,12 @@ do
 
 	for j in `seq 0 $((${#CONDITIONS[@]}-1))`
 	do
-		URL=${URLS[$j]}
 		CONDITION=${CONDITIONS[$j]}
-
-		GFF=$CONDITION.gff
-		if [ ! -e $GFF ]
-			then
-				wget $URL
-				gunzip $GFF.gz
-		fi
 
 		CDT_DIR=$FACTOR_DIR/$CONDITION
 		if [ ! -d $CDT_DIR ]
 			then
-				python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 -o $CDT_DIR $NORM_DIR $GFF
+				python ../scripts/map_shifted_tags_to_ref.py -u 500 -d 500 -o $CDT_DIR $NORM_DIR ../shared_files/$CONDITION.gff
 		fi
 
 		python ../scripts/composite_plots.py -w 20 -y $YLIM $CDT_DIR
