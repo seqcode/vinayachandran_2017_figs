f9ed8876800a13bac1047e60d76e2ab9c9499bce
Lila <lur159@psu.edu>
2017-12-20 15:50:05 -0500

    fixed paths

--- Sup3/sup3b.sh
+++ Sup3/sup3b.sh
@@ -1,36 +1,22 @@
 set -e
 
-sh ../get_chrom_sizes.sh
+sh ../scripts/get_chrom_sizes.sh
 
-SORT_FILE=RP_137_genes_TSS_Xu_2009.gff
-
-if [ ! -e ../shared_files/$SORT_FILE ]
-	then
-		wget 	#TODO: add url
-		mv $SORT_FILE.gz ../shared_files
-		gunzip ../shared_files/$SORT_FILE.gz
-fi
-
-
-ALL_TAB=../GSE98573_RAW
-
-if [ ! -d $ALL_TAB ]
-	then 
-		tar xvf $ALL_TAB.tar
-fi
-
-IDS=(50416 50513 50501 50525 53319 50428 50531 50519 54513 51825 50417 50514 50502 50526 53320 50429 50532 50520 54514 53407)		#TODO: add IDs
 TAB_DIR=tab_files_b
+TAR=GSE98573_RAW.tar
 
 if [ ! -d $TAB_DIR ]
-	then
-		mkdir $TAB_DIR
-
-		for ID in "${IDS[@]}"
-		do
-			cp $ALL_TAB/$ID"sacCer3".tab $TAB_DIR
-		done
+	then 
+		mkdir $TAB_DIR	
+		mv $TAR $TAB_DIR
+		cd $TAB_DIR
+		tar xvf $TAR
+		rm $TAR
+		gunzip *.gz
+		cd ..
 fi
+
+GFF=RP_137_genes_TSS_Xu_2009.gff
 
 NORM_DIR=$TAB_DIR/Normalized_tab_files
 
@@ -38,11 +24,12 @@ if [ ! -d $NORM_DIR ]
 	then
 		python ../scripts/quantile_norm_singlebase_bin.py $TAB_DIR ../shared_files/sacCer3.chrom.sizes
 fi
+
 CDT_DIR=b_CDT
 
 if [ ! -d $CDT_DIR ]
 	then
-		python ../scripts/map_shifted_tags_to_ref.py -u 2000 -d 2000 -o $CDT_DIR tab_files_b/Normalized_tab_files ../shared_files/$SORT_FILE
+		python ../scripts/map_shifted_tags_to_ref.py -u 2000 -d 2000 -o $CDT_DIR tab_files_b/Normalized_tab_files ../shared_files/$GFF
 fi
 
-python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR ../shared_files/$SORT_FILE 
+python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR ../shared_files/$GFF
--- Sup8/sup8.sh
+++ Sup8/sup8.sh
@@ -2,34 +2,21 @@ set -e
 
 sh ../scripts/get_chrom_sizes.sh
 
-ALL_TAB=../GSE98573_RAW
-
-if [ ! -d $ALL_TAB ]
-	then 
-		tar xvf $ALL_TAB.tar
-fi
-
-SORT_FILE=ALL-RP-SAGA-TFIID-SUT-CUT-XUT_TSS-TES-MID_sortedby_geneLength.gff
-
-if [ ! -e ../shared_files/$SORT_FILE ]
-	then
-		wget 	#TODO: add url
-		mv $SORT_FILE.gz ../shared_files
-		gunzip ../shared_files/$SORT_FILE.gz
-fi
-
-IDS=(53816 53814 53811 53809 51836 53815 53812 53810)
 TAB_DIR=tab_files
+TAR=GSE98573_RAW.tar
 
 if [ ! -d $TAB_DIR ]
-	then
-		mkdir $TAB_DIR
-
-		for ID in "${IDS[@]}"
-		do
-			cp $ALL_TAB/$ID"sacCer3".tab $TAB_DIR
-		done
+	then 
+		mkdir $TAB_DIR	
+		mv $TAR $TAB_DIR
+		cd $TAB_DIR
+		tar xvf $TAR
+		rm $TAR
+		gunzip *.gz
+		cd ..
 fi
+
+GFF=ALL-RP-SAGA-TFIID-SUT-CUT-XUT_TSS-TES-MID_sortedby_geneLength.gff
 
 NORM_DIR=$TAB_DIR/Normalized_tab_files
 
@@ -42,7 +29,7 @@ CDT_DIR=CDT
 
 if [ ! -d $CDT_DIR ]
 	then
-		python ../scripts/map_shifted_tags_to_ref.py -u 2000 -d 2000 -o $CDT_DIR $NORM_DIR ../shared_files/$SORT_FILE
+		python ../scripts/map_shifted_tags_to_ref.py -u 2000 -d 2000 -o $CDT_DIR $NORM_DIR ../shared_files/$GFF
 fi
 
-python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR ../shared_files/$SORT_FILE 
+python ../scripts/sort_cdt_by_given_file.py -o 2 $CDT_DIR ../shared_files/$GFF
