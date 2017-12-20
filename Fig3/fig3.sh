4aa8fa2fda8cf9d13369d4f57670723af2b14c70
Lila <lur159@psu.edu>
2017-12-20 16:00:00 -0500

    fixed paths

--- Fig3/fig3.sh
+++ Fig3/fig3.sh
@@ -2,43 +2,23 @@ set -e
 
 sh ../scripts/get_chrom_sizes.sh
 
-ALL_TAB=../GSE98573_RAW
+TAB_DIR=tab_files
+TAR=GSE98573_RAW.tar
 
-if [ ! -d $ALL_TAB ]
+if [ ! -d $TAB_DIR ]
 	then 
-		tar xvf $ALL_TAB.tar
+		mkdir $TAB_DIR	
+		mv $TAR $TAB_DIR
+		cd $TAB_DIR
+		tar xvf $TAR
+		rm $TAR
+		gunzip *.gz
+		cd ..
 fi
 
 SORT_FILE=sorted_list-RPG_SAGA-act-rep-no_TFIID-act-rep-no.txt
 
-if [ ! -e ../shared_files/$SORT_FILE ]
-	then
-		wget 	#TODO: add url
-		mv $SORT_FILE.gz ../shared_files
-		gunzip ../shared_files/$SORT_FILE.gz
-fi
-
 GFF=Xu_2009_RP-SAGA-TFIID_ONLY_TSS_ONLY_V64.gff
-
-if [ ! -e ../shared_files/$GFF ]
-	then
-		wget 	#TODO: add url
-		mv $GFF.gz ../shared_files
-		gunzip ../shared_files/$GFF.gz
-fi
-
-IDS=(50501 50502 50525 50526 51825 53407 53319 53320 50428 50429 50531 50532 50519 50520 54513 54514 53814 53815 53811 53812 53809 53810)
-TAB_DIR=tab_files
-
-if [ ! -d $TAB_DIR ]
-	then
-		mkdir $TAB_DIR
-
-		for ID in "${IDS[@]}"
-		do
-			cp $ALL_TAB/$ID"sacCer3".tab $TAB_DIR
-		done
-fi
 
 NORM_DIR=$TAB_DIR/Normalized_tab_files
 
